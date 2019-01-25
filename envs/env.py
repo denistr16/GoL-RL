35122eaf54ac112f7b09f5f6a6be18cad10b984c
denis_r4 <troyanov.denis@gmail.com>
2019-01-25 11:16:51 +0200

    add field init

--- envs/env.py
+++ envs/env.py
@@ -4,7 +4,10 @@ import numpy as np
 
 class Env:
     def __init__(self, grid_size=(100, 100)):
-        self.grid = np.array(grid_size)
+        self.__dead_cell = 0
+        self.__alive_cell = 1
+
+        self.grid = np.full(grid_size, self.__dead_cell)
 
     def step(self, perception_field: torch.tensor, field_position_x: int, field_position_y: int):
         
