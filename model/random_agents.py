033090f1d9799d86a6ee18c4a8bfd5f73c629886
artem <artemlime.chernov@gmail.com>
2019-01-26 13:32:12 +0200

    Add model with env memory and training process

--- model/random_agents.py
+++ model/random_agents.py
@@ -31,16 +31,24 @@ class RandomPlanter(torch.nn.Module):
         new_points = torch.round(self.model.forward(observations.flatten()))
         return new_points.view(observations.shape[0], observations.shape[1])
 
-class ObserverAndPlanter(torch.nn.Module):
+class MemoryAgentModel(torch.nn.Module):
     def __init__(self, grid_size=100, window_size=10):
         super().__init__()
-
-        self.observer = RandomObserever("Sam", grid_size, window_size)
-        self.planter = RandomPlanter("Mike", window_size)
-        self.window_size = window_size
         self.grid_size = grid_size
+        self.window_size = window_size
+
+        self.memoized_env = torch.nn.Parameter(torch.ones(grid_size, grid_size), requires_grad=False)
+        self.planter = RandomPlanter("Sam",window_size)
+        self.observer = RandomObserever("Mike", grid_size, window_size)
 
     def forward(self, env):
+        
         x, y = self.observer.forward(env)
-        x0, x1, y0, y1 = [x.detach().int(),x.detach().int()+self.window_size, y.detach().int(),y.detach().int()+self.window_size]
-        return self.planter.forward(env[x0:x1, y0:y1]), x0, x1, y0, y1
+
+        x0, x1, y0, y1 = [x.detach().int(),x.detach().int()+self.window_size,
+                          y.detach().int(),y.detach().int()+self.window_size]
+
+        perception_field = self.planter.forward(env[x0:x1, y0:y1])
+        memoized_env = env * self.memoized_env
+        memoized_env[x0:x1, y0:y1] = perception_field
+        return memoized_env, perception_field, x0, y0
