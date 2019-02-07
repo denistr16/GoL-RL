ffa63da85929af7d47078bb303c20a8f829d5a00
artem <artemlime.chernov@gmail.com>
2019-02-07 14:29:06 +0200

    Add reward function for two agents and train notebook for them

--- reward/rewards.py
+++ reward/rewards.py
@@ -4,3 +4,16 @@ class AliveCellsReward:
 
     def forward(self, env):
         return env.sum() / (env.shape[0]*env.shape[1])
+
+import numpy as np
+class MultipleAgentsCellsReward:
+    def __call__(self, env):
+        return self.forward(env)
+
+    def forward(self, env):
+        env = env.flatten()
+        cell_count = np.bincount(env)
+        rewards = [i / sum(cell_count) for i in cell_count[1:]]
+        if len(rewards) == 0:
+            return [0.0, 0.0]
+        return rewards
