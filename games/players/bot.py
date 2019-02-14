282f85dcb910908019e18742f06b74c55352071a
artem <artemlime.chernov@gmail.com>
2019-02-14 13:20:45 +0200

    Add custom perception field size to bot class

--- games/players/bot.py
+++ games/players/bot.py
@@ -9,29 +9,32 @@ def flatten_grid(grid):
 
 
 class BotPlayer:
-    def __init__(self, env, model_path=None, marker=2, max_points_per_step=15):
+    def __init__(self, env, model_path=None, marker=2, max_points_per_step=15, perception_field_size=(5, 5)):
 
-        self.env = env.get_grid()
-        self.model = ActorCritic(self.env.shape[0] * self.env.shape[1],
-                                 self.env.shape[0] * self.env.shape[1])
+        self.env = env.copy()
+
         if model_path is not None:
             snapshot = torch.load(model_path)
             self.model.load_state_dict(snapshot)
 
         self.marker = marker
         self.max_points_per_step = max_points_per_step
+        self.perception_field_size = perception_field_size
 
-    def probs_to_cells(self, probs, env, topk=3, player=1):
+        self.model = ActorCritic(self.env.get_grid().shape[0] * self.env.get_grid().shape[1],
+                                 self.perception_field_size[0]**2)
+
+    def probs_to_cells(self, probs, env, topk=3, marker=1):
         probs_top_k, idx_top_k = probs.topk(topk)
-        inserted_block = np.zeros(env.shape)
+        inserted_block = np.zeros(self.perception_field_size)
         inserted_block = inserted_block.flatten()
-        inserted_block[idx_top_k] = player
-        return inserted_block.reshape(env.shape)
+        inserted_block[idx_top_k] = marker
+        return inserted_block.reshape(self.perception_field_size)
 
-    def step(self):
-        probs = self.model.get_action_probs(flatten_grid(self.env))
+    def step(self, env):
+        probs = self.model.get_action_probs(flatten_grid(env))
         perception_field = self.probs_to_cells(probs=probs.detach(),
-                                            env=self.env,
+                                            env=self.env.get_grid(),
                                             topk=self.max_points_per_step,
                                             player=self.marker)
         return perception_field
