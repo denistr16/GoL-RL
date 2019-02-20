057f1e306d2f1a7e7f4f99fbbd264393717303d7
denis_r4 <troyanov.denis@gmail.com>
2019-02-20 11:39:08 +0200

    add new dir - examples, move notebooks, move run_game.py

--- examples/play_custom_game/run_game.py
+++ examples/play_custom_game/run_game.py
@@ -8,7 +8,8 @@ env = NaiveSandbox((20, 20))
 
 
 if __name__ == "__main__":
-    bot = BotPlayer(model_path='./snapshots/agent_90.pth', env=env, marker=2, max_points_per_step=9, perception_field_size=(4, 4))
+    bot = BotPlayer(model_path='./snapshots/agent_90.pth', env=env, marker=2, max_points_per_step=9,
+                    perception_field_size=(4, 4))
     human = HumanPlayer('artem', env)
 
     GameOfLife(env, human, bot, steps_after_action=20)
